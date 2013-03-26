76d83bd1baee3885daac621686f3a1e0e5182ed1
Brian Trammell <brian@trammell.ch>
2013-03-26 20:18:40 +1300

    beginnings of dynamics-tracking refactor

---
+++ src/qofdyn.c
@@ -0,0 +1,183 @@
+/**
+ ** qofdyn.c
+ ** TCP dynamics tracking data structures and algorithms for QoF
+ **
+ ** ------------------------------------------------------------------------
+ ** Copyright (C) 2013      Brian Trammell. All Rights Reserved
+ ** ------------------------------------------------------------------------
+ ** Authors: Brian Trammell <brian@trammell.ch>
+ ** ------------------------------------------------------------------------
+ ** QoF is made available under the terms of the GNU Public License (GPL)
+ ** Version 2, June 1991
+ ** ------------------------------------------------------------------------
+ */
+
+#define _YAF_SOURCE_
+#include <yaf/autoinc.h>
+
+static const uint64_t startmask64[] = {
+    0xFFFFFFFFFFFFFFFFULL, 0xFFFFFFFFFFFFFFFEULL,
+    0xFFFFFFFFFFFFFFFCULL, 0xFFFFFFFFFFFFFFF8ULL,
+    0xFFFFFFFFFFFFFFF0ULL, 0xFFFFFFFFFFFFFFE0ULL,
+    0xFFFFFFFFFFFFFFC0ULL, 0xFFFFFFFFFFFFFF80ULL,
+    0xFFFFFFFFFFFFFF00ULL, 0xFFFFFFFFFFFFFE00ULL,
+    0xFFFFFFFFFFFFFC00ULL, 0xFFFFFFFFFFFFF800ULL,
+    0xFFFFFFFFFFFFF000ULL, 0xFFFFFFFFFFFFE000ULL,
+    0xFFFFFFFFFFFFC000ULL, 0xFFFFFFFFFFFF8000ULL,
+    0xFFFFFFFFFFFF0000ULL, 0xFFFFFFFFFFFE0000ULL,
+    0xFFFFFFFFFFFC0000ULL, 0xFFFFFFFFFFF80000ULL,
+    0xFFFFFFFFFFF00000ULL, 0xFFFFFFFFFFE00000ULL,
+    0xFFFFFFFFFFC00000ULL, 0xFFFFFFFFFF800000ULL,
+    0xFFFFFFFFFF000000ULL, 0xFFFFFFFFFE000000ULL,
+    0xFFFFFFFFFC000000ULL, 0xFFFFFFFFF8000000ULL,
+    0xFFFFFFFFF0000000ULL, 0xFFFFFFFFE0000000ULL,
+    0xFFFFFFFFC0000000ULL, 0xFFFFFFFF80000000ULL,
+    0xFFFFFFFF00000000ULL, 0xFFFFFFFE00000000ULL,
+    0xFFFFFFFC00000000ULL, 0xFFFFFFF800000000ULL,
+    0xFFFFFFF000000000ULL, 0xFFFFFFE000000000ULL,
+    0xFFFFFFC000000000ULL, 0xFFFFFF8000000000ULL,
+    0xFFFFFF0000000000ULL, 0xFFFFFE0000000000ULL,
+    0xFFFFFC0000000000ULL, 0xFFFFF80000000000ULL,
+    0xFFFFF00000000000ULL, 0xFFFFE00000000000ULL,
+    0xFFFFC00000000000ULL, 0xFFFF800000000000ULL,
+    0xFFFF000000000000ULL, 0xFFFE000000000000ULL,
+    0xFFFC000000000000ULL, 0xFFF8000000000000ULL,
+    0xFFF0000000000000ULL, 0xFFE0000000000000ULL,
+    0xFFC0000000000000ULL, 0xFF80000000000000ULL,
+    0xFF00000000000000ULL, 0xFE00000000000000ULL,
+    0xFC00000000000000ULL, 0xF800000000000000ULL,
+    0xF000000000000000ULL, 0xE000000000000000ULL,
+    0xC000000000000000ULL, 0x8000000000000000ULL,
+};
+
+static const uint64_t endmask64[] = {
+                           0x0000000000000000ULL,
+    0x0000000000000001ULL, 0x0000000000000003ULL,
+    0x0000000000000007ULL, 0x000000000000000FULL,
+    0x000000000000001FULL, 0x000000000000003FULL,
+    0x000000000000007FULL, 0x00000000000000FFULL,
+    0x00000000000001FFULL, 0x00000000000003FFULL,
+    0x00000000000007FFULL, 0x0000000000000FFFULL,
+    0x0000000000001FFFULL, 0x0000000000003FFFULL,
+    0x0000000000007FFFULL, 0x000000000000FFFFULL,
+    0x000000000001FFFFULL, 0x000000000003FFFFULL,
+    0x000000000007FFFFULL, 0x00000000000FFFFFULL,
+    0x00000000001FFFFFULL, 0x00000000003FFFFFULL,
+    0x00000000007FFFFFULL, 0x0000000000FFFFFFULL,
+    0x0000000001FFFFFFULL, 0x0000000003FFFFFFULL,
+    0x0000000007FFFFFFULL, 0x000000000FFFFFFFULL,
+    0x000000001FFFFFFFULL, 0x000000003FFFFFFFULL,
+    0x000000007FFFFFFFULL, 0x00000000FFFFFFFFULL,
+    0x00000001FFFFFFFFULL, 0x00000003FFFFFFFFULL,
+    0x00000007FFFFFFFFULL, 0x0000000FFFFFFFFFULL,
+    0x0000001FFFFFFFFFULL, 0x0000003FFFFFFFFFULL,
+    0x0000007FFFFFFFFFULL, 0x000000FFFFFFFFFFULL,
+    0x000001FFFFFFFFFFULL, 0x000003FFFFFFFFFFULL,
+    0x000007FFFFFFFFFFULL, 0x00000FFFFFFFFFFFULL,
+    0x00001FFFFFFFFFFFULL, 0x00003FFFFFFFFFFFULL,
+    0x00007FFFFFFFFFFFULL, 0x0000FFFFFFFFFFFFULL,
+    0x0001FFFFFFFFFFFFULL, 0x0003FFFFFFFFFFFFULL,
+    0x0007FFFFFFFFFFFFULL, 0x000FFFFFFFFFFFFFULL,
+    0x001FFFFFFFFFFFFFULL, 0x003FFFFFFFFFFFFFULL,
+    0x007FFFFFFFFFFFFFULL, 0x00FFFFFFFFFFFFFFULL,
+    0x01FFFFFFFFFFFFFFULL, 0x03FFFFFFFFFFFFFFULL,
+    0x07FFFFFFFFFFFFFFULL, 0x0FFFFFFFFFFFFFFFULL,
+    0x1FFFFFFFFFFFFFFFULL, 0x3FFFFFFFFFFFFFFFULL, 
+    0x7FFFFFFFFFFFFFFFULL
+};
+
+typedef struct qfSeqBin_st {
+    uint64_t    *bin;
+    size_t      bincount;
+    size_t      opcount;
+    size_t      scale;
+    size_t      binbase;
+    uint32_t    seqbase;
+} qfSeqBin_t;
+
+typedef enum qfSeqBinRes_en {
+    QF_SEQBIN_NO_ISECT,
+    QF_SEQBIN_PART_ISECT,
+    QF_SEQBIN_FULL_ISECT,
+    QF_SEQBIN_OUT_OF_RANGE
+} qfSeqBinRes_t;
+
+void qfSeqBinInit(qfSeqBin_t     *sb,
+                  size_t         capacity,
+                  size_t         scale)
+{
+    /* zero structure */
+    memset(sb, 0, sizeof(*sb));
+    
+    /* allocate bin array */
+    sb->scale = scale;
+    sb->bincount = capacity / scale / (sizeof(uint64_t) * 8);
+    sb->bin = yg_slice_alloc0(sb->bincount * sizeof(uint64_t));
+}
+
+static void qfSeqBinShiftup(qfSeqBin_t     *sb)
+{
+    while (sb->bin[sb->binbase] == UINT64_MAX) {
+        sb->binbase++;
+        if (sb->binbase >= sb->bincount) sb->binbase = 0;
+        sb->seqbase += sb->scale * sizeof(uint64_t) * 8;
+    }
+}
+
+static size_t qfSeqBinNum(qfSeqBin_t   *sb,
+                           uint32_t     seq)
+{
+    return         ((seq - sb->seqbase) /
+            (sizeof(uint64_t) * 8 * sb->scale)) % sb->bincount;
+}
+
+static size_t qfSeqBinBit(qfSeqBin_t    *sb,
+                          uint32_t      seq)
+{
+    return ((seq - sb->seqbase) % (sizeof(uint64_t) * 8 * sb->scale));
+}
+
+static int qfSeqBinAvail(qfSeqBin_t     *sb,
+                         uint32_t       seq)
+{
+    return (seq > sb->seqbase + sb->bincount *
+            sizeof(uint64_t) * 8 * sb->scale) ? 0 : 1;
+}
+
+qfSeqBinRes_t qfSeqBinTestAndSet(qfSeqBin_t      *sb,
+                                 uint32_t        aseq,
+                                 uint32_t        bseq)
+{
+    size_t i, j;
+    
+    /* initialize seqbase if first test and set */
+    if (!(sb->opcount++)) {
+        sb->seqbase = (aseq / sb->scale) * sb->scale;
+    } else {
+        /* range check */
+        if (!qfSeqBinAvail(sb, aseq)) return QF_SEQBIN_OUT_OF_RANGE;
+        if (!qfSeqBinAvail(sb, bseq)) return QF_SEQBIN_OUT_OF_RANGE;
+    }
+    
+    /* get bin numbers */
+    i = qfSeqBinNum(sb, aseq);
+    j = qfSeqBinNum(sb, bseq);
+    
+    /* special case: bins are the same, intersect the masks */
+    if (i == j) {
+        
+    } else {
+        
+        /* handle first bin */
+        
+        /* handle middle bin */
+        
+        /* handle last bin */
+
+    }
+}
+
+typedef struct qfSeqTime_st {
+    uint32_t    lms;
+    uint32_t    seq;
+} qfSeqTime_t;
