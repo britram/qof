c9957e4c15f6995d5ca6944d29ac2d67881822ee
Brian Trammell <brian@trammell.ch>
2013-04-16 09:59:46 +1200

    build checkpoint of (non-integrated) new YAML parser

--- src/qofconfig.c
+++ src/qofconfig.c
@@ -23,13 +23,11 @@
 #define VALBUF_SIZE     80
 #define ADDRBUF_SIZE    42
 
-typedef qfConfig_st {
-    /* Template */
-    // FIXME how to specify template?
+typedef struct qfConfig_st {
     /* Features enabled by template selection */
     gboolean    enable_biflow;  // RFC5103 biflow export 
-    gboolean    enable_ipv4     // IPv4 address export (false = map v4 to v6)
-    gboolean    enable_ipv6     // IPv6 address export (false = drop v6)
+    gboolean    enable_ipv4;    // IPv4 address export (false = map v4 to v6)
+    gboolean    enable_ipv6;    // IPv6 address export (false = drop v6)
     gboolean    enable_mac;     // store MAC addresses and vlan tags
     gboolean    enable_dyn;     // qfdyn master switch (seqno tracking)
     gboolean    enable_dyn_rtx; // qfdyn seqno rtx/loss detection
@@ -85,7 +83,7 @@ static qfConfigKeyAction_t cfg_key_actions[] = {
     {"gre-decap",              CFG_OFF(enable_gre), QF_CONFIG_BOOL},
     {"silk-compatible",        CFG_OFF(enable_silk), QF_CONFIG_BOOL},
     {NULL, NULL, QF_CONFIG_NOTYPE}
-}
+};
 
 static qfConfigKeyAction_t cfg_ie_features[] = {
     {"sourceIPv4Address",       CFG_OFF(enable_ipv4), QF_CONFIG_BOOL},
@@ -103,16 +101,12 @@ static qfConfigKeyAction_t cfg_ie_features[] = {
     {"meanTcpRttMilliseconds",  CFG_OFF(enable_dyn_rtt), QF_CONFIG_BOOL},
     {"maxTcpFlightSize",        CFG_OFF(enable_dyn_rtt), QF_CONFIG_BOOL},
     {NULL, NULL, QF_CONFIG_NOTYPE}
-}
+};
 
 static gboolean qfYamlError(GError                  **err,
                             const yaml_parser_t     *parser,
                             const char              *errmsg)
 {
-    if (!filename) {
-        filename = "<no file given>";
-    }
-    
     if (errmsg) {
         g_set_error(err, YAF_ERROR_DOMAIN, YAF_ERROR_ARGUMENT,
                     "QoF config error: %s at line %zd, column %zd\n",
@@ -165,7 +159,7 @@ static gboolean qfYamlParseKey(yaml_parser_t      *parser,
                                char               *keybuf,
                                size_t             keybuf_sz,
                                GError             **err)
-
+{
     yaml_event_t    event;
 
     /* get event */
@@ -174,7 +168,7 @@ static gboolean qfYamlParseKey(yaml_parser_t      *parser,
     }
 
     /* end of mapping */
-    if ((event.type == YAML_MAPPING_END_EVENT)) return FALSE;
+    if (event.type == YAML_MAPPING_END_EVENT) return FALSE;
 
     /* not scalar */
     if ((event.type != YAML_SCALAR_EVENT)) {
@@ -194,7 +188,7 @@ static gboolean qfYamlParseValue(yaml_parser_t      *parser,
 {
     
     yaml_event_t    event;
-    
+
     /* get event */
     if (!yaml_parser_parse(parser, &event)) {
         return qfYamlError(err, parser, NULL);
@@ -211,7 +205,7 @@ static gboolean qfYamlParseValue(yaml_parser_t      *parser,
     }
     
     /* get string */
-    strncpy(keybuf, (const char*)event.data.scalar.value, keybuf_sz);
+    strncpy(valbuf, (const char*)event.data.scalar.value, valbuf_sz);
     return TRUE;
 }
 
@@ -220,7 +214,6 @@ static gboolean qfYamlParseU32(yaml_parser_t      *parser,
                                   GError          **err)
 {
     char valbuf[VALBUF_SIZE];
-    int rv;
     
     if (!qfYamlParseValue(parser, valbuf, sizeof(valbuf), err)) return FALSE;
 
@@ -237,7 +230,6 @@ static gboolean qfYamlParseU64(yaml_parser_t      *parser,
                                GError             **err)
 {
     char valbuf[VALBUF_SIZE];
-    int rv;
     
     if (!qfYamlParseValue(parser, valbuf, sizeof(valbuf), err)) return FALSE;
     
@@ -247,6 +239,24 @@ static gboolean qfYamlParseU64(yaml_parser_t      *parser,
     
     return TRUE;
 }
+
+static gboolean qfYamlParseBool(yaml_parser_t      *parser,
+                                gboolean           *val,
+                                GError             **err)
+{
+    char valbuf[VALBUF_SIZE];
+    
+    if (!qfYamlParseValue(parser, valbuf, sizeof(valbuf), err)) return FALSE;
+    
+    if (valbuf[0] == '0') {
+        val = FALSE;
+    } else {
+        val = TRUE;
+    }
+    
+    return TRUE;
+}
+
 
 static gboolean qfYamlParsePrefixedV4(yaml_parser_t      *parser,
                                       uint32_t           *addr,
@@ -259,7 +269,7 @@ static gboolean qfYamlParsePrefixedV4(yaml_parser_t      *parser,
     
     if (!qfYamlParseValue(parser, valbuf, sizeof(valbuf), err)) return FALSE;
     
-    rv = sscanf(valbuf, "%15[0-9.]/%hhu", addrbuf, mask));
+    rv = sscanf(valbuf, "%15[0-9.]/%hhu", addrbuf, mask);
     if (rv < 1) {
         return qfYamlError(err, parser, "expected IPv4 address");        
     } else if (rv == 1) {
@@ -277,7 +287,7 @@ static gboolean qfYamlParsePrefixedV4(yaml_parser_t      *parser,
 }
 
 static gboolean qfYamlParsePrefixedV6(yaml_parser_t      *parser,
-                                      uint32_t           *addr,
+                                      uint8_t            *addr,
                                       uint8_t            *mask,
                                       GError             **err)
 {
@@ -287,14 +297,14 @@ static gboolean qfYamlParsePrefixedV6(yaml_parser_t      *parser,
     
     if (!qfYamlParseValue(parser, valbuf, sizeof(valbuf), err)) return FALSE;
     
-    rv = sscanf(valbuf, "%39[0-9a-fA-F:.]/%hhu", addrbuf, mask));
+    rv = sscanf(valbuf, "%39[0-9a-fA-F:.]/%hhu", addrbuf, mask);
     if (rv < 1) {
         return qfYamlError(err, parser, "expected IPv6 address");
     } else if (rv == 1) {
         *mask = 128; // implicit mask
     }
     
-    rv = inet_pton(AF_INET, addrbuf, addr);
+    rv = inet_pton(AF_INET6, addrbuf, addr);
     if (rv == 0) {
         return qfYamlError(err, parser, "invalid IPv6 address");
     } else if (rv == -1) {
@@ -316,8 +326,8 @@ static gboolean qfYamlRequireEvent(yaml_parser_t        *parser,
     }
 
     /* check against expected */
-    if (event_type != et) {
-        return qfYamlError(err, parser, "malformed configuration")
+    if (event.type != et) {
+        return qfYamlError(err, parser, "malformed configuration");
     }
 
     return TRUE;
@@ -328,12 +338,29 @@ static gboolean qfYamlTemplate(qfConfig_t       *cfg,
                                GError           **err)
 {
     char valbuf[VALBUF_SIZE];
+    void *cvp;
+    int i;
     
     /* consume sequence start for template list */
-    if (!qfYamlRequireEvent(parser, YAML_SEQUENCE_START_EVENT)) return FALSE;
+    if (!qfYamlRequireEvent(parser, YAML_SEQUENCE_START_EVENT, err)) {
+        return FALSE;
+    }
+    
+    /* reset the export template */
+    yfWriterExportReset();
 
     while (qfYamlParseValue(parser, valbuf, sizeof(valbuf), err)) {
+        /* Add IE to the export template */
+        if (!yfWriterExportIE(valbuf, err)) return FALSE;
         
+        /* Search IE list for features to enable */
+        for (i = 0; cfg_ie_features[i].key; i++) {
+            if (strncmp(cfg_ie_features[i].key, valbuf, sizeof(valbuf)) == 0) {
+                /* get pointer into cfg and set */
+                cvp = ((void *)cfg) + cfg_ie_features[i].off;
+                *((gboolean *)cvp) = TRUE;
+            }
+        }
     }
 
     /* check for error on last value parse */
@@ -348,10 +375,46 @@ static gboolean qfYamlParseMapEntry(yaml_parser_t *parser,
                                     uint8_t         *v4mask,
                                     uint8_t         *v6addr,
                                     uint8_t         *v6mask,
-                                    uint8_t         *ingress,
-                                    uint8_t         *egress)
+                                    uint32_t        *ingress,
+                                    uint32_t        *egress,
+                                    GError          **err)
 {
-    // FIXME code goes here
+    yaml_event_t event;
+    char keybuf[VALBUF_SIZE];
+    
+    /* get event */
+    if (!yaml_parser_parse(parser, &event)) {
+        return qfYamlError(err, parser, NULL);
+    }
+
+    /* exit on end of enclosing list */
+    if (event.type == YAML_SEQUENCE_END_EVENT) return FALSE;
+    
+    /* error if not mapping start */
+    if (event.type != YAML_MAPPING_START_EVENT) {
+        return qfYamlError(err, parser, "malformed configuration");
+    }
+    
+    /* parse key - value pairs */
+    while (qfYamlParseKey(parser, keybuf, sizeof(keybuf), err)) {
+        if (strncmp("ip4-net", keybuf, sizeof(keybuf))) {
+            if (!qfYamlParsePrefixedV4(parser, v4addr, v4mask, err)) return FALSE;
+        } else if (strncmp("ip6-net", keybuf, sizeof(keybuf))) {
+            if (!qfYamlParsePrefixedV6(parser, v6addr, v6mask, err)) return FALSE;
+        } else if (strncmp("ingress", keybuf, sizeof(keybuf))) {
+            if (!qfYamlParseU32(parser, ingress, err)) return FALSE;
+        } else if (strncmp("egress", keybuf, sizeof(keybuf))) {
+            if (!qfYamlParseU32(parser, egress, err)) return FALSE;
+        } else {
+            return qfYamlError(err, parser, "unknown interface map parameter");
+        }
+    }
+    
+    /* check for error on last key parse */
+    if (*err) return FALSE;
+    
+    /* done */
+    return TRUE;
 }
 
 static gboolean qfYamlInterfaceMap(qfConfig_t       *cfg,
@@ -360,23 +423,31 @@ static gboolean qfYamlInterfaceMap(qfConfig_t       *cfg,
 {
     uint32_t    v4addr = 0;
     uint8_t     v6addr[16];
+    uint8_t     v4mask, v6mask;
+    uint32_t    ingress, egress;
     
     /* consume sequence start for interface-map mapping list */
-    if (!qfYamlRequireEvent(parser, YAML_SEQUENCE_START_EVENT)) return FALSE;
+    if (!qfYamlRequireEvent(parser, YAML_SEQUENCE_START_EVENT, err)) {
+        return FALSE;
+    }
     
     memset(v6addr, 0, sizeof(v6addr));
     
     /* consume map entries until we see a sequence end */
     while (qfYamlParseMapEntry(parser, &v4addr, &v4mask,
-                               &v6addr, &v6mask, &ingress, &egress, err))
+                               v6addr, &v6mask, &ingress, &egress, err))
     {
+        if ((ingress > 255) || (egress > 255)) {
+            qfYamlError(err, parser, "interface too large");
+        }
+        
         if (v4addr) {
-            qfIfMapAddIPv4Mapping(&cfg->map, v4addr, v4mask, ingress, egress);
+            qfIfMapAddIPv4Mapping(&cfg->ifmap, v4addr, v4mask, ingress, egress);
             v4addr = 0;
         }
         
         if (v6addr[0]) {
-            qfIfMapAddIPv6Mapping(&cfg->map, v6addr, v6mask, ingress, egress);
+            qfIfMapAddIPv6Mapping(&cfg->ifmap, v6addr, v6mask, ingress, egress);
             memset(v6addr, 0, sizeof(v6addr));
         }
     }
@@ -401,9 +472,12 @@ static gboolean qfYamlDocument(qfConfig_t       *cfg,
     char keybuf[VALBUF_SIZE];
     
     /* consume events to get us into the main document map */
-    if (!qfYamlRequireEvent(parser, YAML_STREAM_START_EVENT)) return FALSE;
-    if (!qfYamlRequireEvent(parser, YAML_DOCUMENT_START_EVENT)) return FALSE;
-    if (!qfYamlRequireEvent(parser, YAML_MAPPING_START_EVENT)) return FALSE;
+    if (!qfYamlRequireEvent(parser, YAML_STREAM_START_EVENT, err) ||
+        !qfYamlRequireEvent(parser, YAML_DOCUMENT_START_EVENT, err) ||
+        !qfYamlRequireEvent(parser, YAML_MAPPING_START_EVENT, err))
+    {
+        return FALSE;        
+    }
     
     /* eat keys and dispatch actions */
     while (qfYamlParseKey(parser, keybuf, sizeof(keybuf), err)) {
@@ -411,24 +485,24 @@ static gboolean qfYamlDocument(qfConfig_t       *cfg,
         keyfound = 0;
         
         /* check for template */
-        if (strncmp("template", keybuf, sizeof(keybuf) {
+        if (strncmp("template", keybuf, sizeof(keybuf)) == 0) {
             if (!qfYamlTemplate(cfg, parser, err)) return NULL;
             keyfound = 1;
         }
         
         /* check interface map */
-        if (strncmp("interface-map", keybuf, sizeof(keybuf) {
+        if (strncmp("interface-map", keybuf, sizeof(keybuf)) == 0) {
             if (!qfYamlInterfaceMap(cfg, parser, err)) return NULL;
             keyfound = 1;
         }
         
         /* nope; check action list */
         for (i = 0; (!keyfound) && cfg_key_actions[i].key; i++) {
-            if (strncmp(keybuf, cfg_key_actions[i].key, sizeof(keybuf)) == 0) {
+            if (strncmp(cfg_key_actions[i].key, keybuf, sizeof(keybuf)) == 0) {
                 
                 /* get pointer into cfg, parse based on type */
                 cvp = ((void *)cfg) + cfg_key_actions[i].off;
-                switch (actions[i].vp) {
+                switch (cfg_key_actions[i].vt) {
                   case QF_CONFIG_U32:
                     rv = qfYamlParseU32(parser, (uint32_t*)cvp, err);
                     break;
@@ -439,7 +513,8 @@ static gboolean qfYamlDocument(qfConfig_t       *cfg,
                     rv = qfYamlParseBool(parser, (gboolean*)cvp, err);
                     break;
                   default:
-                    return qfYamlError(err, parser, "internal error in map dispatch");
+                    return qfYamlError(err, parser,
+                                       "internal error in map dispatch");
                 }
 
                 /* check for error */
@@ -459,9 +534,12 @@ static gboolean qfYamlDocument(qfConfig_t       *cfg,
 
     /* consume events to get us back out of the stream
        (note the end of the ParseKey loop consumed the mapping end) */
-    if (!qfYamlRequireEvent(parser, YAML_DOCUMENT_END_EVENT)) return FALSE;
-    if (!qfYamlRequireEvent(parser, YAML_STREAM_END_EVENT)) return FALSE;
-
+    if (!qfYamlRequireEvent(parser, YAML_DOCUMENT_END_EVENT, err) ||
+        !qfYamlRequireEvent(parser, YAML_STREAM_END_EVENT, err))
+    {
+        return FALSE;
+    }
+                    
     return TRUE;
 }
 
@@ -469,5 +547,34 @@ gboolean qfConfigParseYaml(qfConfig_t           *cfg,
                            const char           *filename,
                            GError               **err)
 {
+    yaml_parser_t       parser;
+    FILE                *file;
+    gboolean            rv = TRUE;
     
+    // short circuit on no file
+    if (!filename) return TRUE;
+    
+    // open file and initialize parser
+    if (!(file = fopen(filename,"r"))) {
+        g_set_error(err, YAF_ERROR_DOMAIN, YAF_ERROR_ARGUMENT,
+                    "cannot open YAML configuration file %s: %s",
+                    filename, strerror(errno));
+        return FALSE;
+    }
+    
+    memset(&parser, 0, sizeof(parser));
+    if (!yaml_parser_initialize(&parser)) {
+        return qfYamlError(err, &parser, NULL);
+    }
+    
+    yaml_parser_set_input_file(&parser, file);
+    
+    /* parse the document */
+    if (!qfYamlDocument(cfg, &parser, err)) rv = FALSE;
+    
+    /* clean up and close file */
+    yaml_parser_delete(&parser);
+    fclose(file);
+    
+    return rv;
 }
