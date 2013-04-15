9efd12a6d9077c0db95f7c295e07aeba628cf67c
Brian Trammell <brian@trammell.ch>
2013-04-15 17:04:21 +1200

    (non-building) EOD commit of new configuration parser

--- src/qofconfig.c
+++ src/qofconfig.c
@@ -46,27 +46,31 @@ typedef qfConfig_st {
     uint32_t    max_fragtab;
     uint64_t    max_flow_pkt;     // max packet count to force ATO (silk mode)
     uint64_t    max_flow_oct;     // max octet count to force ATO  (silk mode)
-    uint32_t    ato_rtt_multiple; // multiple of RTT to force ATO
+    uint32_t    ato_rtts;         // multiple of RTT to force ATO
     /* TCP dynamics state configuration */
     uint32_t    rtt_samples;
-    uint32_t    rtx_distance;
+    uint32_t    rtx_span;
     uint32_t    rtx_scale;
     /* Interface map */
     qfIfMap_t   ifmap;
 } qfConfig_t;
 
 typedef enum {
+    QF_CONFIG_NOTYPE = 0,
     QF_CONFIG_STRING,
     QF_CONFIG_U32,
-    QF_CONFIG_U64
+    QF_CONFIG_U64,
+    QF_CONFIG_BOOL,
+    QF_CONFIG_FUNC,
 } qfConfigKeyType_t;
 
 typedef struct qfConfigKeyAction_st {
     char                *key;
     void                *vp;
     qfConfigKeyType_t   vt;
-    (gboolean)          *parse_fn(qfConfig_t*, yaml_parser_t*, GError**);
 } qfConfigKeyAction_t;
+
+typedef (gboolean) *qfConfigParseFn(qfConfig_t*, yaml_parser_t*, GError**);
 
 static gboolean qfYamlError(GError                  **err,
                             const yaml_parser_t     *parser,
@@ -163,10 +167,8 @@ static gboolean qfYamlParseValue(yaml_parser_t      *parser,
         return qfYamlError(err, parser, NULL);
     }
     
-    /* end of mapping or sequence */
-    if ((event.type == YAML_MAPPING_END_EVENT) ||
-        (event.type == YAML_SEQUENCE_END_EVENT))
-    {
+    /* end of sequence is okay */
+    if (event.type == YAML_SEQUENCE_END_EVENT) {
         return FALSE;
     }
     
@@ -269,18 +271,51 @@ static gboolean qfYamlParsePrefixedV6(yaml_parser_t      *parser,
     return TRUE;
 }
 
+static gboolean qfYamlRequireEvent(yaml_parser_t        *parser,
+                                   yaml_event_type_t    et,
+                                   GError               **err)
+{
+    yaml_event_t event;
+
+    /* get event */
+    if (!yaml_parser_parse(parser, &event)) {
+        return qfYamlError(err, parser, NULL);
+    }
+
+    /* check against expected */
+    if (event_type != et) {
+        return qfYamlError(err, parser, "malformed configuration")
+    }
+
+    return TRUE;
+}
+
 static gboolean qfYamlTemplate(qfConfig_t       *cfg,
                                yaml_parser_t    *parser,
                                GError           **err)
 {
+    char valbuf[VALBUF_SIZE];
+    
+    /* consume events to get us into the template list */
+    if (!qfYamlRequireEvent(parser, YAML_SEQUENCE_START_EVENT)) return FALSE;
 
+    while (qfYamlParseValue(parser, valbuf, sizeof(valbuf), err)) {
+        // FIXME add logic to search a list of enable-able things
+        
+    }
+
+    /* check for error on last value parse */
+    if (*err) return FALSE;
+
+    /* done */
+    return TRUE;
 }
 
 static gboolean qfYamlInterfaceMap(qfConfig_t       *cfg,
                                    yaml_parser_t    *parser,
                                    GError           **err)
 {
-    
+
 }
 
 
@@ -288,13 +323,85 @@ static gboolean qfYamlDocument(qfConfig_t       *cfg,
                                yaml_parser_t    *parser,
                                GError           **err)
 {
+    // FIXME base this on offsets instead
     qfConfigKeyAction_t actions[] = {
-        {"active-timeout", &(cfg->ato_ms), QF_CONFIG_U32, NULL},
-        {"idle-timeout",   &(cfg->ito_ms), QF_CONFIG_U32, NULL},
-        {NULL, NULL, QF_CONFIG_NOTYPE, NULL}
+        {"active-timeout",      &(cfg->ato_ms), QF_CONFIG_U32},
+        {"idle-timeout",        &(cfg->ito_ms), QF_CONFIG_U32},
+        {"max-flows",           &(cfg->max_flowtab), QF_CONFIG_U32},
+        {"max-frags",           &(cfg->max_fragtab), QF_CONFIG_U32},
+        {"active-timeout-octets",  &(cfg->max_flow_oct), QF_CONFIG_U64},
+        {"active-timeout-packets", &(cfg->max_flow_pkt), QF_CONFIG_U64},
+        {"active-timeout-rtts",    &(cfg->ato_rtts), QF_CONFIG_U32},
+        {"rtx-span",        &(cfg->rtx_span), QF_CONFIG_U32},
+        {"rtx-scale",       &(cfg->rtx_scale), QF_CONFIG_U32},
+        {"rtt-samples",     &(cfg->rtt_samples), QF_CONFIG_U32},
+        {"gre-decap",       &(cfg->enable_gre), QF_CONFIG_BOOL},
+        {"silk-compatible", &(cfg->enable_silk), QF_CONFIG_BOOL},
+        {"interface-map",   qfYamlInterfaceMap, QF_CONFIG_FUNC},
+        {"template",        qfYamlTemplate, QF_CONFIG_FUNC},
+        {NULL, NULL, QF_CONFIG_NOTYPE}
     }
     
+    int i, keyfound;
+    gboolean rv;
+    void *cvp;
     
+    char keybuf[VALBUF_SIZE];
+    
+    /* consume events to get us into the main document map */
+    if (!qfYamlRequireEvent(parser, YAML_STREAM_START_EVENT)) return FALSE;
+    if (!qfYamlRequireEvent(parser, YAML_DOCUMENT_START_EVENT)) return FALSE;
+    if (!qfYamlRequireEvent(parser, YAML_MAPPING_START_EVENT)) return FALSE;
+    
+    /* eat keys and dispatch actions */
+    while (qfYamlParseKey(parser, keybuf, sizeof(keybuf), err)) {
+        keyfound = 0;
+        for (i = 0; (!keyfound) && actions[i].key; i++) {
+            if (strncmp(keybuf, actions[i].key, sizeof(keybuf)) == 0) {
+                
+                /* add action offset to cfg base pointer */
+                cvp = ((void *)cfg) + actions[i].vp;
+                
+                switch (actions[i].vp) {
+                  case QF_CONFIG_U32:
+                    rv = qfYamlParseU32(parser, (uint32_t*)cvp, err);
+                    break;
+                  case QF_CONFIG_U64:
+                    rv = qfYamlParseU64(parser, (uint64_t*)cvp, err);
+                    break;
+                  case QF_CONFIG_BOOL:
+                    rv = qfYamlParseBool(parser, (gboolean*)cvp, err);
+                    break;
+                  case QF_CONFIG_FUNC:
+                    rv = (qfConfigParseFn)actions[i].vp(cfg, parser, err);
+                    break;
+                  default:
+                    return qfYamlError(err, parser, "internal error in map dispatch");
+                }
+
+                /* check for error on value parse */
+                if (!rv) {
+                    return FALSE;
+                }
+                
+                /* good value, jump to next key */
+                keyfound = 1;
+            }
+        }
+        
+        /* end of for loop, unknown key */
+        return qfYamlError(err, parser, "unknown configuration parameter");
+    }
+    
+    /* check for error on last key parse */
+    if (*err) return FALSE;
+
+    /* consume events to get us back out of the stream
+       (note the end of the ParseKey loop consumed the mapping end) */
+    if (!qfYamlRequireEvent(parser, YAML_DOCUMENT_END_EVENT)) return FALSE;
+    if (!qfYamlRequireEvent(parser, YAML_STREAM_END_EVENT)) return FALSE;
+
+    return TRUE;
 }
 
 gboolean qfConfigParseYaml(qfConfig_t           *cfg,
@@ -302,5 +409,4 @@ gboolean qfConfigParseYaml(qfConfig_t           *cfg,
                            GError               **err)
 {
     
-}
 }
