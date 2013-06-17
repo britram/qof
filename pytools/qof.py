6fb847e0393f87a42e0fd635260233efbabc7c82
Brian Trammell <trammell@tik.ee.ethz.ch>
2013-06-17 13:48:05 +0200

    tweaks to pytools

--- pytools/qof.py
+++ pytools/qof.py
@@ -17,6 +17,31 @@ def dataframe_from_ipfix(filename, *ienames):
         r = ipfix.reader.from_stream(f)
         df = pd.DataFrame.from_records(
             [rec for rec in r.records_as_tuple(ielist)],
-            columns = [ie.name or ie in ielist])
+            columns = [ie.name for ie in ielist])
         return df
-    
+
+count_ies = ("octetDeltaCount", 
+             "packetDeltaCount",
+             "initiatorOctets", 
+             "initiatorPackets",
+             "tcpSequenceCount",
+             "tcpSequenceLossCount")
+
+rtt_ies = ("meanTcpRttMilliseconds",
+           "minTcpRttMilliseconds")
+
+rtx_ies = ("tcpRetransmitCount", 
+           "tcpRtxBurstCount")
+
+rev_count_ies = ("reverseOctetDeltaCount", 
+                 "reversePacketDeltaCount",
+                 "responderOctets",
+                 "responderPackets",
+                 "reverseTcpSequenceCount",
+                 "reverseTcpSequenceLossCount")
+
+rev_rtt_ies = ("reverseMeanTcpRttMilliseconds",
+               "reverseMinTcpRttMilliseconds")
+
+rev_rtx_ies ("reverseTcpRetransmitCount",
+             "reverseTcpRtxBurstCount")
